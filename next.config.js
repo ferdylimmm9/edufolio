6da251f079a4dc7b401e45efd058b6df3924d7c4
Ferdy Lim <ferdy.lim@springkraf.com>
2024-07-17 16:51:24 +0700

    feat: pwa

--- next.config.js
+++ next.config.js
@@ -1,4 +1,16 @@
 // const { withSentryConfig } = require('@sentry/nextjs');
+const runtimeCaching = require('@ducanh2912/next-pwa').runtimeCaching;
+const withPWA = require('@ducanh2912/next-pwa').default({
+  dest: 'public',
+  workboxOptions: {
+    runtimeCaching,
+    maximumFileSizeToCacheInBytes: 10485760, // increase to 10 MB
+  },
+  cacheOnFrontEndNav: true,
+  aggressiveFrontEndNavCaching: true,
+  disable: process.env.NODE_ENV !== 'production',
+  dynamicStartUrl: false,
+});
 const { createVanillaExtractPlugin } = require('@vanilla-extract/next-plugin');
 const { PHASE_DEVELOPMENT_SERVER } = require('next/constants');
 const withTranslation = require('next-translate-plugin');
@@ -45,7 +57,7 @@ const nextConfig = {
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 module.exports = async (phase, { defaultConfig }) => {
   const isDev = phase === PHASE_DEVELOPMENT_SERVER;
-  const plugins = [withTranslation, withVanillaExtract];
+  const plugins = [withTranslation, withVanillaExtract, withPWA];
   if (!isDev) {
     // plugins.push(withSentryConfig);
   }
